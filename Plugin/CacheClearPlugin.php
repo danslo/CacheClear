274300a1dcfc39f289eb5db661abbcde130167c4
Daniel Sloof <daniel@cream.nl>
2016-09-23 09:29:32 +0200

    consistent naming

--- Plugin/CacheClearPlugin.php
+++ Plugin/CacheClearPlugin.php
@@ -2,10 +2,12 @@
 
 namespace Danslo\CacheClear\Plugin;
 
-class ClearCachePlugin
+class CacheClearPlugin 
 {
 
     /**
+     * Clears a cache type when it becomes invalid.
+     *
      * @param \Magento\Framework\App\Cache\TypeListInterface $subject
      * @param callable $proceed
      * @param ...$args
@@ -13,6 +15,6 @@ class ClearCachePlugin
     public function aroundInvalidate(\Magento\Framework\App\Cache\TypeListInterface $subject, callable $proceed, ...$args)
     {
         $proceed(...$args);
-        $subject->cleanType($args[0]);
+        $subject->cleanType(...$args);
     }
 }
